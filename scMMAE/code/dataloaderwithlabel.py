9fd2826a6333fbecb5ed0133a690b991707d0933
DM <89619779+DM0815@users.noreply.github.com>
2024-01-30 19:15:38 +0800

    Update dataloaderwithlabel.py

--- scMMAE/code/dataloaderwithlabel.py
+++ scMMAE/code/dataloaderwithlabel.py
@@ -14,4 +14,20 @@ class MultiModalDataset_label(Dataset):
         mod2_feature = self.mod2_data[idx]
         label = self.labels[idx]
         return {'mod1': mod1_feature, 'mod2': mod2_feature, 'label': label}
+
+##Dataloader  single modality
+class SingleModalDataset(Dataset):
+    def __init__(self, mod1_data, labels):
+        self.mod1_data = mod1_data
+        #self.mod2_data = mod2_data
+        self.labels = labels
+
+    def __len__(self):
+         return len(self.mod1_data)
+
+    def __getitem__(self, idx):
+        mod1_feature = self.mod1_data[idx]
+        #mod2_feature = self.mod2_data[idx]
+        label = self.labels[idx]
+        return {'mod1': mod1_feature, 'label': label}
     
